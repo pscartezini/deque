5ad88eb66f3166f979a9d5383775f89cda3f3a3f
Paulo Rodrigo Scartezini <pauloscartezini@gmail.com>
2016-03-07 15:53:37 -0300

    Ae carralho barra de progresso funcionando e sumindo no waypoint 1 com delay de 3 segundos

--- Assets/scripts/Gatilho.cs
+++ Assets/scripts/Gatilho.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class Gatilho : MonoBehaviour {
 	public bool permission = true;
 	public bool doIt = false;
-	public int timer= 3;
+	public float timer=0;
+	 public Canvas CanvasObject;
+	public Image barraDeProgresso;
+
+
+
 	void OnTriggerEnter(Collider other) {
 		
 		doIt = false;
@@ -13,18 +18,31 @@ public class Gatilho : MonoBehaviour {
 
 	void Update(){
 		if(doIt){
-			Debug.Log("Looking at");
-			 GameObject player = GameObject.Find("CardboardMain");
-			 player.transform.position = Vector3.MoveTowards(player.transform.position, transform.position, 3.0f*Time.deltaTime);
+			timer+= Time.deltaTime;
+			if(timer>0){
+				   CanvasObject.enabled = true;
+			}
+			Debug.Log("Looking at"+timer);
+			barraDeProgresso.fillAmount = (timer*1)/3;
+			if(timer>=3){
+				CanvasObject.enabled = false;
+				permission=false;
+				GameObject player = GameObject.Find("CardboardMain");
+			 	player.transform.position = Vector3.MoveTowards(player.transform.position, transform.position, 3.0f*Time.deltaTime);
+			}
+			 
 		}
 		if(!doIt){
 			Debug.Log("Looking away");
+			timer= 0;
+				CanvasObject.enabled = false;
 		}
 	}
 
 	public void change(bool status){
 		if(permission){
 			doIt=status;
+			
 			//permission= false;
 		}
 
