eb38f660c4b7d2df1e6880fa23f75b0bb9dd70f6
Paulo Rodrigo Scartezini <pauloscartezini@gmail.com>
2016-03-08 09:18:42 -0300

    arrumado segundo waypoint e desligada a luz da cozinha

--- Assets/scripts/Gatilho2.cs
+++ Assets/scripts/Gatilho2.cs
@@ -1,24 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class Gatilho2 : MonoBehaviour {
 	public bool permission = true;
 	public bool doIt = false;
+	public float timer=0;
+	 public Canvas CanvasObject;
+	public Image barraDeProgresso;
+
+
+
 	void OnTriggerEnter(Collider other) {
-		Debug.Log("Okay");
-        doIt=false;
+		
+		doIt = false;
+        //gameObject.SetActive(false);
 	}
 
 	void Update(){
 		if(doIt){
-			 GameObject player = GameObject.Find("CardboardMain");
-			 player.transform.position = Vector3.MoveTowards(player.transform.position, transform.position, 3.0f*Time.deltaTime);
+			timer+= Time.deltaTime;
+			if(timer>0){
+				   CanvasObject.enabled = true;
+			}
+			Debug.Log("Looking at"+timer);
+			barraDeProgresso.fillAmount = (timer*1)/3;
+			if(timer>=3){
+				CanvasObject.enabled = false;
+				permission=false;
+				GameObject player = GameObject.Find("CardboardMain");
+			 	player.transform.position = Vector3.MoveTowards(player.transform.position, transform.position, 3.0f*Time.deltaTime);
+			}
+			 
+		}
+		if(!doIt){
+			Debug.Log("Looking away");
+			timer= 0;
+				CanvasObject.enabled = false;
 		}
 	}
 
 	public void change(bool status){
 		if(permission){
 			doIt=status;
+			
 			//permission= false;
 		}
 
